8dabcaf7fc3a07503a85ae7da0263bf39ccfaa25
HGNEO <130890333+HGNEO@users.noreply.github.com>
2025-06-23 19:12:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 rm -rf .repo/local_manifests; \
 repo init -u https://github.com/WitAqua/manifest.git -b 15.2 --git-lfs; \
 /opt/crave/resync.sh; \
+/opt/crave/resync.sh; \
 
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250; \
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
@@ -19,6 +20,7 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi; 
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+
 . build/envsetup.sh; \
 breakfast munch; \
 brunch munch
